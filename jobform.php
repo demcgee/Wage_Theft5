9aecd94dddb55c342194e1fe9d19639b275b054e
ykim39 <yujeong-kim@uiowa.edu>
2016-05-04 16:53:08 -0500

    Update jobform.php

--- jobform.php
+++ jobform.php
@@ -32,7 +32,7 @@
 
 <html lang="en">
 <head>
-  <title>Wage Theft</title>
+  <title>Stop! Wage Theft</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="runnable.css" />
@@ -47,7 +47,7 @@
 <nav class="navbar navbar-default">
   <div style = "background:#A9D0F5 !important" class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="Home.php">Wage Theft</a>
+      <a class="navbar-brand" href="Home_in.php">font face="Arial Black">Stop! Wage Theft</a></font>
     </div>
     <ul class="nav navbar-nav">
       <li><a href="Home_in.php"><span style="font-size:1.0em" class="glyphicon glyphicon-home"></span><font face="Arial Black"> Home</a></li></font>

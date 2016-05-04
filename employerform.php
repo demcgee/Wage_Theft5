795eb673eb69b37f631819ec508b8f53343b2ab4
ykim39 <yujeong-kim@uiowa.edu>
2016-05-04 15:53:25 -0500

    Update employerform.php

--- employerform.php
+++ employerform.php
@@ -23,19 +23,23 @@
 <nav class="navbar navbar-default">
   <div style = "background:#A9D0F5 !important" class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="Home.php">Wage Theft</a>
-    </div>
+      <a class="navbar-brand" href="Home.php"><font face="Arial Black" >Stop! Wage Theft</a></font>
+	</div>
+<font face="arial black">
     <ul class="nav navbar-nav">
-      <li class= "active"><a href="Home.php">Home</a></li>
-      <li><a href="enterhours.php">Enter Hours</a></li>
-	  <li><a href="enterpaycheck.php">Enter Paycheck</a></li>
-      <li><a href="faq.php">FAQ</a></li>
+      <li><a href="Home.php"><span style="font-size:1.0em" class="glyphicon glyphicon-home"></span><font face="Arial Black"> Home</a></li></font>
+	  <li><a href="jobform.php"><span style="font-size:1.0em" class="glyphicon glyphicon-briefcase"></span><font face="Arial Black"> Enter Job</a></li></font>
+      <li><a href="enterhours.php"><span style="font-size:1.0em" class="glyphicon glyphicon-time"></span><font face="Arial Black"> Enter Hours</a></li></font>
+      <li><a href="enterpaycheck.php"><span style="font-size:1.0em" class="glyphicon glyphicon-barcode"></span><font face="Arial Black"> Enter Paycheck</a></li></font>
+      <li><a href="Makeclaim.php"><span style="font-size:1.0em" class="glyphicon glyphicon-bullhorn"></span><font face="Arial Black"> Make Claim</a></li></font>
+      <li><a href="faq.php"><span style="font-size:1.0em" class="glyphicon glyphicon-question-sign"></span><font face="Arial Black"> FAQ</a></li></font>
+      <li><a href="contactus.php"><span style="font-size:1.0em" class="glyphicon glyphicon-phone-alt"></span><font face="Arial Black"> Contact Us</a></li></font>
     </ul>
-  </div>
+	</div>
 </nav>
   
 <div class="container">
-  <h3>Enter Employer Information</h3>
+  <h2>Enter Employer Information</h2>
 </div>
 
 <?php
@@ -46,7 +50,6 @@
 if (isset($_POST['E_NAME'])) {
 //	echo '<p>we are processing form data</p>';
 //	print_r($_POST);
-
 	// get data from the input fields
 	$E_NAME = $_POST['E_NAME'];
 	$ADDRESS = $_POST['ADDRESS'];
@@ -56,15 +59,12 @@ if (isset($_POST['E_NAME'])) {
 	if (!$E_NAME) {
 		punt ("Please enter a name");
 	}
-
 	if (!$ADDRESS) {
 		punt("Please enter an address");
 	}
-
 	if (!$E_EMAIL) {
 		punt("Please enter an email");
 	}
-
 	// check if address is already in database
 		// connect to database
 	$db = connectDB($DBHost,$DBUser,$DBPasswd,$DBName);
@@ -95,6 +95,7 @@ if (isset($_POST['E_NAME'])) {
     echo "\t\tThe employer " . $E_NAME . " was added to the database\n";
 	echo "</div></div>\n";
 	
+	header('Location: inputuser.php');
 }
 ?>
 
@@ -117,28 +118,31 @@ if (isset($_POST['E_NAME'])) {
 	<input type="email" class="form-control" name="E_EMAIL"/>
 </div>
   
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+  <button type="submit" class="btn btn-default" onclick="myFunction()">Submit</button>
+ <script>
+function myFunction(){
+	alert("Employer information is saved successfully!");
+}
+</script>
+</form><br><br>
 
 <!----------------->
 <!---List employers--->
 <!----------------->
-<div class="container">
+
 <div class="col-xs-12">
 	<h2><?php echo "Employer"; ?></h2>
-</div>
-</div>
 
-<div class="container">
-<div class="col-xs-12">
-<table class="table table-hover">
+</div>
 
 <!-- Titles for table -->
-<thead>
-<tr>
-	<th>Name</th>
+<table class="table table-hover">
+
+<tr style="background:#ECCEF5 !important">
+
+	<td>Name</td>
 </tr>
-</thead>
+
 
 <tbody>
 <?php

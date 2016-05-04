fb8552c364ffbcf80441a2f50850f3faf7a6a34b
Benschroder <benjamin-schroder@uiowa.edu>
2016-05-04 15:21:59 -0500

    Update db.sh

--- db.sh
+++ db.sh
@@ -2,4 +2,4 @@
 
 # Change DBPASSWORD, DBUSER, DBHOST and DBNAME to match the values
 # your mysql_db_info file on the webdev server.
-mysql --password='5ZUcsP=4HmVE' --user='ykim39' --host='dbdev.cs.uiowa.edu' 'db_ykim39'
+mysql --password='_6p-CIKHAGct' --user='bschroder' --host='dbdev.cs.uiowa.edu' 'db_bschroder'
